567dde6477547b4b5943ef4f7a1d14dcd8f7fa31
BradleyLamitie <Bradley.Lamitie1@marist.edu>
2016-09-20 19:34:33 -0400

    ReAdded Lab3- errored copy
    ReAdded Lab3 to fix the strange error shown in error.png
    -contains an error on purpose

---
+++ DatabaseManagementLab3.sql
@@ -0,0 +1,64 @@
+--List the order	number and total dollars of	all	orders.	--
+
+SELECT    ordnum, totalusd potato
+FROM      orders;
+
+--List the name and city of agents named Smith--
+
+SELECT    name, city
+FROM      agents
+WHERE     name='Smith';
+
+--List the id, name, and priceUSD of products with quantity more than 201,000--
+
+SELECT     pid, name, priceusd
+FROM       products
+WHERE      quantity > 201000;
+
+--List the names and cities of customers in Duluth--
+
+SELECT     name, city
+FROM       customers
+WHERE      city = 'Duluth';
+
+--List the names of agents not in New York and not in Duluth--
+
+SELECT     name
+FROM       agents
+WHERE      city!= 'Duluth' 
+                AND
+           city!= 'New York';
+
+--List all data for products in neither Dallas nor Duluth that cost US $1 or more--
+
+SELECT     *
+FROM       products
+WHERE      city!= 'Dallas'
+               AND
+           city!= 'Duluth'
+               AND 
+           priceusd >= 1;
+
+--List all data for orders in February or March--
+
+SELECT     *
+FROM       orders
+WHERE      mon='feb'
+		      OR
+           mon = 'mar';
+
+--List all data for orders in February of US $600 or more--
+
+SELECT     *
+FROM       orders
+WHERE      mon = 'feb'
+              AND 
+           totalusd >= 600;
+
+--List all data from the customer whose cid is C005--
+
+SELECT     *
+FROM       orders
+WHERE      cid= 'c005';
+
+
